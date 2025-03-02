fe4789d63ef83336bb42b1e750039f8136b7c6c5
AleksandarM <amarkovic29@gmail.com>
2025-03-02 16:05:44 +0100

    refactor

--- routers/auth.py
+++ routers/auth.py
@@ -1,8 +1,56 @@
-from fastapi import APIRouter
+from http.client import HTTPException
+from typing import Annotated
+
+import status
+from fastapi import APIRouter,Depends
+from pydantic import BaseModel, Field
+from starlette import status
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from models import Users
+from passlib.context import CryptContext
+
 
 router = APIRouter()
 
-@router.get("/auth")
-async def get_user():
-    return {"user","authenticated"}
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session,Depends(get_db)]
+
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+
+@router.post("/auth",status_code=status.HTTP_201_CREATED)
+async def create_user(db:db_dependency,create_user_request: CreateUserRequest):
+     create_user_model = Users(username=create_user_request.username,
+                               email=create_user_request.email,
+                               first_name=create_user_request.first_name,
+                               last_name=create_user_request.last_name,
+                               role=create_user_request.role,
+                               hashed_password=bcrypt_context.hash(create_user_request.password),
+                               is_active=True)
+
+     db.add(create_user_model)
+     db.commit()
+
+
+
+
+
 
